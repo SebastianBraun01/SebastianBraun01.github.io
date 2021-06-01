9b5d2c1f5aa679cdd77531e0da363ed2260cf0e4
SebastianBraun01 <SebastianBraun-Ost@protonmail.com>
2021-06-01 23:13:37 +0200

    artikel verbessert

--- ts/navigation.ts
+++ ts/navigation.ts
@@ -1,16 +1,16 @@
 const dropdown = document.getElementById("dropdown");
-const dropdown_arrow = document.getElementById("dropdown_arrow");
+const dropdown_arrow = document.getElementById("dropdown_icon");
 const haupt_nav = document.getElementById("haupt-nav");
 const sub_nav = document.getElementById("sub-nav");
 
 dropdown.onclick = () => {
-  if(dropdown_arrow.innerText == "˃"){
-    dropdown_arrow.innerText = "˅";
+  if(dropdown_arrow.innerText == "+"){
+    dropdown_arrow.innerText = "-";
     haupt_nav.className = "";
     sub_nav.className = "schatten";
   }
   else{
-    dropdown_arrow.innerText = "˃";
+    dropdown_arrow.innerText = "+";
     haupt_nav.className = "schatten";
     sub_nav.className = "unsichtbar";
   }
