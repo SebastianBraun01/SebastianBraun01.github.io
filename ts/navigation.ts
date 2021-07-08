34c21d57b43599a753815c8b033ec2dfe9684b2d
SebastianBraun01 <SebastianBraun-Ost@protonmail.com>
2021-07-08 12:49:59 +0200

    handy freundliche navbar

--- ts/navigation.ts
+++ ts/navigation.ts
@@ -4,33 +4,49 @@ if (document.title == "Home | Sebastian Braun") path = "./pages";
 else path = "../pages";
 
 document.getElementById("haupt-nav").insertAdjacentHTML("afterend", `
-<nav id="sub-nav" class="unsichtbar">
+<nav id="sub-nav" class="active">
   <a class="nav-link" href="${path}/led_beleuchtung.html">LED Beleuchtung</a>
   <a class="nav-link" href="${path}/led_matrix.html">LED Matrix</a>
   <a class="nav-link" href="${path}/nixie_uhr.html">Nixie Uhr</a>
   <a class="nav-link" href="${path}/rustuino.html">Rustuino</a>
+</nav>
+
+<nav id="mobile-nav">
+  <a class="nav-link" href="#" id="mobile-dropdown">+Menu</a>
 </nav>
 `);
 
 
 // Navbar toggle ==================================================================================
 const dropdown = document.getElementById("dropdown");
-const dropdown_icon = document.getElementById("dropdown_icon");
+const mobile_dropdown = document.getElementById("mobile-dropdown");
 const haupt_nav = document.getElementById("haupt-nav");
 const sub_nav = document.getElementById("sub-nav");
 
 dropdown.onclick = () => {
-  if (dropdown_icon.innerText == "+"){
-    dropdown_icon.innerText = "-";
-    haupt_nav.classList.remove("schatten");
+  if (dropdown.innerText == "+Projekte"){
+    dropdown.innerText = "-Projekte";
     haupt_nav.style.boxShadow = "none";
-    sub_nav.style.display = "flex";
+    sub_nav.classList.remove("active");
   }
   else{
-    dropdown_icon.innerText = "+";
+    dropdown.innerText = "+Projekte";
+    sub_nav.classList.add("active");
     haupt_nav.style.boxShadow = "5px 5px 8px #B6B6B6"
-    sub_nav.style.display = "none";
   }
+}
+
+mobile_dropdown.onclick = () => {
+  if (mobile_dropdown.innerText == "+Menu") {
+    mobile_dropdown.innerText = "-Menu";
+    haupt_nav.classList.remove("active");
+    sub_nav.classList.remove("active");
+  }
+  else {
+    mobile_dropdown.innerText = "+Menu";
+    haupt_nav.classList.add("active");
+    sub_nav.classList.add("active");
+  } 
 }
 
 
